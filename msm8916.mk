6919c5dc070ec2e8a01b8631b708154d8f0e094d
jro1979oliver <jroliveira.oliveira301@gmail.com>
2020-06-14 17:16:46 +0000

    msm8916-common: Build aosp vibrator service

--- msm8916.mk
+++ msm8916.mk
@@ -298,7 +298,8 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-service.lineage
+    android.hardware.vibrator@1.0-service \
+    android.hardware.vibrator@1.0-impl
 
 # Wifi
 PRODUCT_PACKAGES += \
